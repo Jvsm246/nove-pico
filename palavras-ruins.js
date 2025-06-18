c3d2de7bcfb67856714f214a8e54692502811022
Jvsm246 <j.magro@escola.pr.gov.br>
2025-06-18 09:14:01 -0300

    poi99789

---
+++ palavras-ruins.js
@@ -0,0 +1,5 @@
+const palavrasComuns = [
+  "a", "à", "o", "e", "de", "do", "da", "dos", "das", "em", "um", "uma", "que", "como", "para", "por", "com", "se",
+  "não", "sim", "eu", "você", "ele", "ela", "nós", "eles", "elas", "foi", "ser", "tem", "têm", "são", "era", "mas",
+  "ou", "na", "no", "nos", "nas", "já", "mais", "menos", "há", "os", "as"
+];
